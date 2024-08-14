e17f66821de70d0908cf2251550eda55fa6c9fd0
VolodumurGruv <gru.v@i.ua>
2024-08-15 00:31:36 +0300

    changed app.component.html

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, social-animals');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Social animals project'
+    );
   });
 });
