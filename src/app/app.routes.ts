459d7b81b118dfb77e44303744d997a465ae5371
Bohdan Zhykhor <bohdanzhyhor@gmail.com>
2024-08-17 13:08:19 -0400

    added pages Home, Registration, Page not found, added component header; updated html in app component; made working navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', component: HomeComponent },
+  { path: 'registration', component: RegistrationComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
