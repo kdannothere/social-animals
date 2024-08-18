a638551ec710aaf4ae3f0e173fb46b8cdd67e894
Bohdan Zhykhor <bohdanzhyhor@gmail.com>
2024-08-17 22:50:32 -0400

    created initial form on Registration page, added possibility to import style files easily, added reset.css to reset settings for all browsers, added colors.scss for storing all colors used in project

--- src/app/pages/registration/registration.component.ts
+++ src/app/pages/registration/registration.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registration.component.html',
-  styleUrl: './registration.component.scss'
+  styleUrl: './registration.component.scss',
 })
 export class RegistrationComponent {
+  constructor(private fb: FormBuilder) {}
 
+  regForm = this.fb.group({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+      Validators.maxLength(128),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(128),
+    ]),
+    confirmPassword: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(128),
+    ]),
+  });
+  submit() {
+    if (!this.regForm.valid) {
+      alert('Some fields are not correct.');
+      return;
+    }
+    const controls = this.regForm.controls;
+    if (controls.password.value !== controls.confirmPassword.value) {
+      alert('Password and confirm password should be the same.');
+      return;
+    }
+    const formsData = this.regForm.getRawValue();
+    //register(formsData);
+  }
 }
