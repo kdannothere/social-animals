a02604dcad5b1d3d24ed9179e8e53704e0b10337
VolodumurGruv <gru.v@i.ua>
2024-08-31 03:23:34 +0300

    footer and header styling are done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,11 @@ import { Routes } from '@angular/router';
 import { LayoutComponent } from './shared/components/layout/layout.component';
 
 export const routes: Routes = [
-  {path: ' ', component: LayoutComponent,
+  {path: '', component: LayoutComponent,
     children: [
-      {path: '', redirectTo: '/', pathMatch: "full"},
       {path: '', loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent)}
     ]
   },
   { path: 'registration', loadComponent: () => import('./pages/registration/registration.component').then(c => c.RegistrationComponent) },
-  { path: '**', redirectTo: 'notfound', loadComponent: () => import('./pages/page-not-found/page-not-found.component').then(c => c.PageNotFoundComponent) },
+  { path: '**', loadComponent: () => import('./pages/page-not-found/page-not-found.component').then(c => c.PageNotFoundComponent) },
 ];
