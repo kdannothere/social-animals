4482b06005893ce89621836aa41e05d71609b704
Bohdan Zhykhor <bohdanzhyhor@gmail.com>
2024-09-01 02:07:32 -0400

    implemented navigation in header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,28 +1,27 @@
-
 import { Component, HostListener } from '@angular/core';
 import { NgClass } from '@angular/common';
 
 import { DropArrowsIconComponent } from '../drop-arrows-icon/drop-arrows-icon.component';
-import { navData } from '../config/navData';
-
-
-
+import { navData, getLink } from '../config/navData';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [DropArrowsIconComponent, NgClass],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  logoUrl = "../../../scss/lib/pics/logo.png";
-  svgUrl = "../../../scss/lib/pics/svg/arrow-down.svg";
-  logoAlt = "чотирилапі герої";
+  logoUrl = '../../../scss/lib/pics/logo.png';
+  svgUrl = '../../../scss/lib/pics/svg/arrow-down.svg';
+  logoAlt = 'чотирилапі герої';
   navData = navData;
+  getLink = getLink;
   isUaActive = true;
   isEnActive = false;
-/*
+
+
+  /*
   @HostListener('document:click', ['$event']) onClick(event: Event): void {
     const target = event.target as HTMLElement;
     console.log(target.innerText)
@@ -31,26 +30,23 @@ export class HeaderComponent {
   }
 */
 
-
-  openClose (id: number | null) {
-    this.navData.forEach(item => {
-      if(item.id === id) {
-
+  openClose(id: number | null) {
+    this.navData.forEach((item) => {
+      if (item.id === id) {
         item.isOpened = !item.isOpened;
       } else {
         item.isOpened = false;
       }
-    })
+    });
   }
 
-
   currentLang(lang: string) {
-    if(lang === "ua") {
+    if (lang === 'ua') {
       this.isUaActive = true;
       this.isEnActive = false;
     }
 
-    if(lang === "en") {
+    if (lang === 'en') {
       this.isEnActive = true;
       this.isUaActive = false;
     }
