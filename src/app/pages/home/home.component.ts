e626b32f77c94d99d55f9700d1cdefaab3926e76
Paul <ploppasha@gmail.com>
2024-09-11 15:38:53 +0300

    home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-
+import { BlockJoinTeamComponent } from '../../blocks/block-join-team/block-join-team.component';
+import { OrangeDotPartionComponent } from '../../shared/components/orange-dot-partion/orange-dot-partion.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ ],
+  imports: [BlockJoinTeamComponent, OrangeDotPartionComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
